9841d2579ae8e7892b6cd598724d99f592539e9c
Lee Jeong Seong <kou8148@naver.com>
2021-12-22 11:16:53 +0900

    add combination

--- src/algorithm/Combination.java
+++ src/algorithm/Combination.java
@@ -2,23 +2,34 @@
 package algorithm;
 
 public class Combination {
-	static void combination(int depth, int n, int[] arr,int count) {
-		if(depth==arr.length) {
-			System.out.println(count);
+	static void combination(int[] arr, boolean[] visited, int start, int n, int r) {
+		if(r==0) {
+			print(arr,visited,n);
+			return;
 		}
-		for(int i=depth; i<arr.length; i+=n) {
-			count++;
+		for(int i=start; i<n; i++) {
+			visited[i]=true;
+			combination(arr,visited,i+1,n,r-1);
+			visited[i]=false;
 		}
-		combination(depth+1,n,arr,count);
-	}
+	} // end of static combination
+	
+	static void print(int[] arr, boolean[] visited, int n) {
+		for(int i=0; i<n; i++) {
+			if(visited[i]) {
+				System.out.print(arr[i]+" ");
+			}
+		}
+		System.out.println();
+	} // end of print
 	
 	public static void main(String[] args) {
-		int depth = 0;
-		int n = 2;
 		int[] arr = {1,2,3,4};
-		int count=0;
+		int n = arr.length;
+		boolean[] visited = new boolean[n];
+		int r = 2;
 		
-		combination(depth,n,arr,count);
-		// n개 중에서 a개 뽑는 경우의 수
+			System.out.println("%s개 중에서 %s개 뽑기".formatted(n,r));
+			combination(arr,visited,0,n,r);
 	} // end of main
 }
