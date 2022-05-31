53c56a0d05118de8c3c91ec9e11f88c101166c5a
Lee Jeong Seong <kou8148@naver.com>
2022-05-31 13:12:54 +0900

    남은 것 추가

---
+++ src/algorithm/programmers/level3/ExpressingAsN.java
@@ -0,0 +1,20 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42895
+
+package algorithm.programmers.level3;
+
+public class ExpressingAsN {
+
+	public static void main(String[] args) {
+		int N = 5;
+		int number = 12;
+		int answer = 0; // 4
+		// =====================================
+
+		
+		
+		if(answer > 8) {
+			answer = -1;
+		}
+	}
+
+}
